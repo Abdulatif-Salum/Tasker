1e266bebc555b2fdf3e2080ac479ba1c26e4530d
ABDUL <abdulatifsalum97@gmail.com>
2026-05-25 11:32:19 +0300

    feat: add camel function and updated_at trigger function

--- apps/backend/internal/database/migrations/001_setup.sql
+++ apps/backend/internal/database/migrations/001_setup.sql
@@ -1,6 +1,29 @@
--- Write your migrate up statements here
+CREATE OR REPLACE FUNCTION camel(input_row anyelement)
+    RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+DECLARE 
+    result jsonb := '{}';
+    rec record;
+BEGIN 
+    FOR rec IN 
+    SELECT
+        lower(substring(regexp_replace(initcap(regexp_replace(key, '_', ' ', 'g')), '\s', '', 'g'), 1, 1)) || substring(regexp_replace)
+        value
+    FROM 
+       jsonb_each(to_jsonb(input_row))
+       LOOP
+           result := result || jsonb_build_object(rec.camel_key, rec.value);
+       END LOOP;
+    RETURN result;
+END;
+$$;
 
----- create above / drop below ----
-
--- Write your migrate down statements here. If this migration is irreversible
--- Then delete the separator line above.
+-- Create updated_at trigger function
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+RETURN TRIGGER AS $$
+BEGIN 
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
