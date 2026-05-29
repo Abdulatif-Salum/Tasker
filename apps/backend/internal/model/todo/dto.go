af907de50c0ae64a014db000e90bbd8ac2d1c5dd
ABDUL <abdulatifsalum97@gmail.com>
2026-05-29 15:33:42 +0300

    feat: add CreateTodoPayload struct with validation

---
+++ apps/backend/internal/model/todo/dto.go
@@ -0,0 +1,27 @@
+package todo
+
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+//-----------------------------------------------------
+
+type CreateTodoPayload struct{
+	  Title           string    `json:"title"  validate:"required,min=1,max=255"`
+		Description    *string    `json:"description" validate:"omitempty,max=1000"`
+    Priority       *Priority  `json:"priority"  validate:"omitempty,oneof=low medium high"`
+		DueDate        *time.Time `json:"dueDate"`
+		ParentTodoID   *uuid.UUID `json:"parentTodoId" validate:"omitempty,uuid"`
+		CategoryID     *uuid.UUID `json:"categoryId" validate:"omitempty,uuid"`
+		Metadata       *Metadata  `json:"metadata"`
+}
+
+func (p *CreateTodoPayload) Validate() error{
+	  validate := validator.New()
+		return validate.Struct(p)
+}
+
+//-----------------------------------------------------
